ec6cb4d50f89f724a046eaf9b7879619fdd59eac
Leonardo Nascimento <omgleo@live.com>
2023-02-15 22:01:45 -0300

    primeiro controller test

--- src/main/java/br/com/projeto/api/controller/Controler.java
+++ src/main/java/br/com/projeto/api/controller/Controler.java
@@ -1,5 +1,14 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controler {
     
+    @GetMapping("/")
+    public String teste(){
+        return "hello world";
+    }
+
 }
