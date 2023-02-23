9df60e726152a286945f50d622e43aa447cda60f
Leonardo Nascimento <omgleo@live.com>
2023-02-22 22:01:26 -0300

    Annotation @Autowired

--- src/main/java/br/com/projeto/api/controller/Controler.java
+++ src/main/java/br/com/projeto/api/controller/Controler.java
@@ -1,11 +1,19 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.api.repository.Repositorio;
+
 @RestController
 public class Controler {
-    
+   
+  @Autowired 
+  private Repositorio repositorio;
+
+
+
     @GetMapping("/")
     public String teste(){
         return "hello world";
