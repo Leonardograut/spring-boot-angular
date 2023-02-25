2d891d3647d8ab2c9b3910e04fb0d188b186bedc
Leonardo Nascimento <omgleo@live.com>
2023-02-25 20:52:20 -0300

    Rota de Cadastro

--- src/main/java/br/com/projeto/api/controller/Controler.java
+++ src/main/java/br/com/projeto/api/controller/Controler.java
@@ -2,8 +2,11 @@ package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.api.model.Cliente;
 import br.com.projeto.api.repository.Repositorio;
 
 @RestController
@@ -13,6 +16,10 @@ public class Controler {
   private Repositorio repositorio;
 
 
+  @PostMapping("/")
+  public Cliente cadastrar(@RequestBody Cliente c){
+    return repositorio.save(c);
+  }
 
     @GetMapping("/")
     public String teste(){
