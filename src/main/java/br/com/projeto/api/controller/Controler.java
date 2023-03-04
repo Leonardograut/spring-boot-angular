9a634c399ce8a0793ae74121e53325c428f95462
Leonardo Nascimento <omgleo@live.com>
2023-03-03 22:00:44 -0300

    Rota de remoção

--- src/main/java/br/com/projeto/api/controller/Controler.java
+++ src/main/java/br/com/projeto/api/controller/Controler.java
@@ -1,7 +1,9 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,5 +35,9 @@ public class Controler {
       return repositorio.save(c);
     }
 
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id ){
+        repositorio.deleteById(id);
+    }
 
 }
