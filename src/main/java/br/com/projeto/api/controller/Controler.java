89a1e4160a824fcedeacfadaadfdeb4f5a90ca73
Leonardo Nascimento <omgleo@live.com>
2023-03-11 20:20:15 -0300

    adicionando requestmapping

--- src/main/java/br/com/projeto/api/controller/Controler.java
+++ src/main/java/br/com/projeto/api/controller/Controler.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.model.Cliente;
 import br.com.projeto.api.repository.Repositorio;
 
+@RequestMapping("/clientes")
 @RestController
 @CrossOrigin(origins = "*")
 public class Controler {
