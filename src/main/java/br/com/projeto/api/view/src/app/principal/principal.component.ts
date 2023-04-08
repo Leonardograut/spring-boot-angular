89773ee69d58aef82b905e1ac69e9fac5aaa7f75
Leonardo Nascimento <omgleo@live.com>
2023-04-07 23:38:12 -0300

    Listagem de clientes (Parte 2)

--- src/main/java/br/com/projeto/api/view/src/app/principal/principal.component.ts
+++ src/main/java/br/com/projeto/api/view/src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent {
-
+  
+  
+  //Variavel para visibilade dos botoes
   btnCadastro:boolean =true;
+
+  //JSON de clientes
+  clientes:Cliente[] = [];
+
+  
+constructor(private servico:ClienteService){
+
+}
+
+  //Metodo de seleçao
+  selecionar():void{
+
+    this.servico.selecionar()
+    .subscribe(retorno =>this.clientes =retorno);
+  }
 }
